ac627fe6b4eaa585ea64234d886d0b8dfbc62747
Thorsten Seyschab <business@todde.tv>
2024-11-12 17:53:15 +0100

    feat: add custom VClick component

---
+++ components/CustomVClick.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useNav } from '@slidev/client'
+
+const props = defineProps<{
+  showOnClick: number
+}>()
+
+const { clicks } = useNav()
+</script>
+
+<template>
+  <slot v-if="clicks === props.showOnClick" />
+</template>
+
+<style scoped>
+</style>
